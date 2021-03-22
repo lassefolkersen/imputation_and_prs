63d1f93d8d257a554e1a886929971673e0546086
Lasse Folkersen <lwf@ngc.dk>
2021-03-22 22:19:18 +0100

    testing different setups for singularity running

--- Docker_as_singularity/2021-03-22_making_docker_container_work_on_cluster.sh
+++ Docker_as_singularity/2021-03-22_making_docker_container_work_on_cluster.sh
@@ -27,3 +27,55 @@ singularityImage=~/impute-me_v1.0.04.sif
 # Execution halted
 
 #odd, this is supposed to work. Auto-loading check_for_cron_ready_jobs
+
+#ok try with the --writebale and the --cleanenv
+cd ~
+mkdir temp
+
+
+echo "prepare_individual_genome('/home/ubuntu/input/HaplotypeCaller_synt0046.vcf.gz')" > test.R
+
+$singularityBinary shell --cleanenv --bind /ngc/people/lwf/temp:/home/ubuntu/data --writable --overlay /ngc/people/lwf/temp $singularityImage 
+
+$singularityBinary image.create my-overlay.img
+
+singularity exec --cleanenv -B $HOME/script_out:/dir myimage.sif bash script.sh
+
+
+
+$singularityBinary exec --bind /ngc/people/lwf:/home/ubuntu/data --writable $singularityImage Rscript /home/ubuntu/srv/impute-me/imputeme/vcf_handling_cron_job.R
+$singularityBinary exec --bind /ngc/people/lwf/temp:/home/ubuntu/data --writable $singularityImage Rscript /home/ubuntu/data/test.R
+$singularityBinary exec --bind /ngc/people/lwf/temp:/home/ubuntu/data --writable $singularityImage Rscript /home/ubuntu/data/test.R
+
+
+
+
+$singularityBinary exec \
+--bind /ngc/people/lwf/temp/vcfs:/home/ubuntu/vcfs,/ngc/people/lwf/temp/data:/home/ubuntu/data \
+$singularityImage Rscript /home/ubuntu/data/test.R
+#doesn't work because of permissions
+
+$singularityBinary exec \
+--bind /ngc/people/lwf/temp/vcfs:/home/ubuntu/vcfs,/ngc/people/lwf/temp/data:/home/ubuntu/data \
+$singularityImage Rscript /home/ubuntu/data/test.R
+$singularityBinary exec --bind /ngc/people/lwf/temp/vcfs:/home/ubuntu/vcfs,/ngc/people/lwf/temp/data:/home/ubuntu/data $singularityImage ls /home/ubuntu
+#doesn't work because of permissions
+
+
+$singularityBinary exec \
+--bind /ngc/people/lwf/temp/vcfs:/home/ubuntu/vcfs,\
+/ngc/people/lwf/temp/data:/home/ubuntu/data,\
+/ngc/people/lwf/temp/misc_files:/home/ubuntu/misc_files,\
+/ngc/people/lwf/temp/logs:/home/ubuntu/logs,\
+/ngc/people/lwf/temp:/home/ubuntu/input \
+$singularityImage Rscript /home/ubuntu/input/test.R
+#this actually works - apparently needs to mount every folder in the docker
+#that needs writing. Otherwise the singularity won't run as non-admin
+#ok, let's try to make that a bit smoother
+
+
+
+
+
+
+
